3477dc31ce62248f7a1d10db6a84a0b2a08e9fc8
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-07-12 18:25:16 -0300

    sequelize importing

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
+const sequelize = require('./services/db');
 
 const userRouter = require('./routes/user');
 
-
+app.use(express.json());
 app.use('/api/v1/users',userRouter);
 
-app.listen(port, ()=>{
-    console.log('listening on port ', port);
+sequelize.sync()
+.then(result=>{
+    app.listen(port, ()=>{
+        console.log('listening on port ', port);
+    })
+})
+.catch(err =>{
+    console.log('err: ', err)
 })
