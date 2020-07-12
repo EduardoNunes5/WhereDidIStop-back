708f0ca50ba61c4ab36cfdb468de3d64ffb357a7
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-07-12 18:23:34 -0300

    getuserbyid ok

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,8 +1,16 @@
-
-exports.getUsers = (req, res, next)=>{
-    res.send('users');
+const userRepository = require('../repository/user');
+exports.getUsers = async(req, res, next)=>{
+    const users =  await userRepository.getUsers();
+    if(users.length > 0){
+        return res.status(200).json(users);
+    }
+    res.status(204).json(users);
 }
 
-exports.getUser = (req, res, next)=>{
-    res.send(req.params.id);
+exports.getUser = async(req, res, next)=>{
+    const user = await userRepository.getById(req.params.id);
+    if(user){
+        return res.status(200).json(user);
+    }
+    res.status(204).json(user);
 }
