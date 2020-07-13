ce2fb615daad7e5a36f8d66640604c9dee82e3d8
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-07-13 16:29:09 -0300

    sign up method created

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,7 @@
 const userRepository = require('../repository/user');
+const bcrypt = require('bcrypt');
+
+
 exports.getUsers = async(req, res, next)=>{
     const users =  await userRepository.getUsers();
     if(users.length > 0){
@@ -8,7 +11,7 @@ exports.getUsers = async(req, res, next)=>{
 }
 
 exports.getUser = async(req, res, next)=>{
-    const user = await userRepository.getById(req.params.id);
+    const user = await userRepository.getByEmail(req.params.email);
     if(user){
         return res.status(200).json(user);
     }
@@ -16,11 +19,19 @@ exports.getUser = async(req, res, next)=>{
 }
 
 
-exports.postUser = async(req, res, next)=>{
-    console.log(req.body);
-    const name = req.body.name;
-    //to treat later
-    const password = req.body.password;
-    const user = await userRepository.postUser({name, password});
-    res.status(201).json(user);
+exports.signUp = async(req, res, next)=>{
+    try{
+        const {name, email, password} = req.body;
+        const hashedPassword = await bcrypt.hash(password,10);
+        console.log(hashedPassword);
+        let user = await userRepository.getByEmail(email);
+        if(user){
+            return res.status(409).json({msg: 'User with such email address already exists'})
+        }
+        user = await userRepository.postUser({name, email, hashedPassword});
+        res.status(201).json(user);
+    }
+    catch(err){
+        res.status(500).json({msg: 'Internal error: ' + err});
+    }
 }
