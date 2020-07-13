bf05b9bdcca49ca304071b05be39eef71383f22a
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-07-13 19:31:48 -0300

    authorization middleware

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -54,3 +54,21 @@ exports.login = async (req, res, next)=>{
         res.status(500).json({msg: "erro: " + err})
     }
 }
+
+exports.isAuthorized = async (req, res, next) =>{
+    const authHeader = req.headers.authorization
+    const token = authHeader && authHeader.split(" ")[1];
+    if(!token){
+        return res.status(401).json({msg: "oops! no token has been provided"});
+    }
+    //either err or the decoded token
+    const canAccess = await auth.verifyToken(token);
+    if(!access)
+        return res.status(401).json({msg: "wrong authentication credentials"})
+    else{
+        req.user = canAccess;
+        next()
+    }
+
+
+}
