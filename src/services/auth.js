d3dda74eae31460e9d6b2cc89952002e43b1e47e
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-07-13 19:32:04 -0300

    verify token

--- src/services/auth.js
+++ src/services/auth.js
@@ -12,6 +12,6 @@ exports.genToken = async (userId)=>{
     return await jwt.sign({user: userId}, process.env.ACCESS_TOKEN_SECRET);
 }
 
-// exports.authenticateToken (token)=>{
-//
-// }
+exports.verifyToken = async(token)=>{
+    return await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET) 
+}
