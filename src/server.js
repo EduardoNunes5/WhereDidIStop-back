79b87c2cb9806ad3045496af4b1539bc5b3c1192
Eduardo Nunes <50140955+EduardoNunes5@users.noreply.github.com>
2020-07-14 18:41:46 -0300

    switch acess to access

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use((req,res,next)=>{
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Acess-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 })
 app.use('/api/',authRouter);
