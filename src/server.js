07887924c5810bc46638da3e83391ff510819afe
Eduardo Nunes <50140955+EduardoNunes5@users.noreply.github.com>
2020-07-15 20:09:45 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use((req,res,next)=>{
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
     next();
 })
 app.use('/api/',authRouter);
