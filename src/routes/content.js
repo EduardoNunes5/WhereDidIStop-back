bac276c863cdd6ac0ff01563d0cb0be565375086
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-07-17 22:53:05 -0300

    update content route added

--- src/routes/content.js
+++ src/routes/content.js
@@ -6,5 +6,6 @@ const routes = express.Router();
 
 routes.post('/contents', auth.isAuthorized, contentController.postContent);
 routes.get('/contents', auth.isAuthorized, contentController.getContents);
+routes.put('/contents/:id', auth.isAuthorized, contentController.putContent);
 
 module.exports = routes;
