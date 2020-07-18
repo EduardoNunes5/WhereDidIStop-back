63304e30e0822ed5421c576fabce5809070a9e24
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-07-17 22:51:54 -0300

    change message if not authenticated

--- src/middleware/isAuth.js
+++ src/middleware/isAuth.js
@@ -4,7 +4,7 @@ exports.isAuthorized = async (req, res, next) =>{
     const authHeader = req.headers.authorization
     const token = authHeader && authHeader.split(" ")[1];
     if(!token){
-        return res.status(401).json({msg: "oops! no token has been provided"});
+        return res.status(401).json({msg: "Not authenticated"});
     }
     //either err or the decoded token
      auth.verifyToken(token)
