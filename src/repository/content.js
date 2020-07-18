6774d62fd9184c4834c41273565d1461b60ecc70
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-07-17 22:53:30 -0300

    update method implemented

--- src/repository/content.js
+++ src/repository/content.js
@@ -1,4 +1,22 @@
 const Content = require('../models/content.js');
 
 exports.postContent = async(content) => await Content.create(content);
-exports.getUserContents = async(userId) => await Content.findAll({where:{user_pk: userId}})
+
+exports.getUserContents = async(userId) => await Content.findAll({where:{user_pk: userId}});
+
+exports.getById = async(contentId) => await Content.findOne({where:{id: contentId}});
+
+exports.updateContent = async(contentId, payload) => {
+    const content = await this.getById(contentId);
+    if(content){
+        return content.update(payload,{
+            where:{
+                user_pk: payload.ownerId,
+                id: contentId
+                }
+            })
+    }
+    else{
+        return content;
+    }
+}
