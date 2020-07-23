2a1673119d52b82776482689386887230b6f26b9
Eduardo Nunes <50140955+EduardoNunes5@users.noreply.github.com>
2020-07-23 13:24:36 -0300

    allow methods

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ app.use(bodyParser.json());
 app.use((req,res,next)=>{
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
+    res.header('Acess-Control-Allow-Methods', 'POST, DELETE, PUT, GET');
     next();
 })
 app.use('/api/',authRouter);
